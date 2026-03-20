cc3a9760a345211c79bbe7d4f74c0efeb4cfdcdf
m68k <matei.toader12@gmail.com>
2026-03-20 17:20:12 +0200

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'm68k';
+export const SITE_DESCRIPTION = 'Student. Write-ups, projects and things I find interesting.';
