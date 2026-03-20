9eff4dc050111daff723236d35791d9159a4365f
m68k <matei.toader12@gmail.com>
2026-03-20 19:01:16 +0200

    feat: add custom domain m68k.ro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://toadermatei.github.io',
+	site: 'https://m68k.ro',
 	integrations: [mdx(), sitemap()],
 });
