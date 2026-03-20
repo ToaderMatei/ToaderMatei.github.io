7a75f773d0a977ea63986f4622a26a788cf1fe3a
m68k <matei.toader12@gmail.com>
2026-03-20 17:55:21 +0200

    feat: redesign homepage and about, add tags to post list

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'm68k';
-export const SITE_DESCRIPTION = 'Student. Write-ups, projects and things I find interesting.';
+export const SITE_DESCRIPTION = 'CTF write-ups and security research by m68k.';
